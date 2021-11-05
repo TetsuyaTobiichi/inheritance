ae5bf4e8cdc8ca59f7e8f14243741e19d96121af
TetsuyaTobiichi <91623795+TetsuyaTobiichi@users.noreply.github.com>
2021-11-05 17:21:35 +0800

    Добавьте файлы проекта.

---
+++ inheritance/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace inheritance
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Gadget> techList = new List<Gadget>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Sum();
+        }
+        private void fill_Click(object sender, RoutedEventArgs e)
+        {
+            techList.Clear();
+            var rnd = new Random();
+            for(byte i=0; i < 15; i++)
+            {
+                switch (rnd.Next() % 3)
+                {
+                    case 0:
+                        techList.Add(new Laptops());
+                        break;
+                    case 1:
+                        techList.Add(new Smartphone());
+                        break;
+                    case 2:
+                        techList.Add(new Tablet());
+                        break;
+                }
+            }
+            Sum();
+        }
+        protected void Sum()
+        {
+            byte phones = 0, laptops = 0, tablet = 0;
+            foreach(var tech in techList)
+            {
+                if(tech is Laptops)
+                {
+                    laptops++;
+                }
+                else if(tech is Tablet)
+                {
+                    tablet++;
+                }
+                else if(tech is Smartphone)
+                {
+                    phones++;
+                }
+            }
+            infoBox.Text = $"кол-во ноутбуков {laptops} кол-во планшетов {tablet} кол-во смартфонов {phones}";
+        }
+        private void Take_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.techList.Count == 0)
+            {
+                infoBox.Text = "Автомат пуст";
+            }
+            else
+            {
+                var temp = this.techList[0];
+                this.techList.RemoveAt(0);
+                takeInfoBox.Text = temp.getInfo();
+                Sum();
+            }
+        }
+    }
+}
---
+++ inheritance/classes.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace inheritance
+{
+    public class Gadget
+    {
+        protected static Random rnd = new Random();
+        protected int resolution = 0;
+        public virtual string getInfo()
+        {
+            this.resolution = 240 + 120 * (rnd.Next() % 3);
+            return $"разрешение экрана {resolution}";
+        }
+
+    }
+    public class Laptops : Gadget
+    {
+        byte cores = 0;
+        int memory = 0;
+        string bluetooth = string.Empty;
+        public Laptops()
+        {
+            this.cores = (byte)(6 + rnd.Next() % 7);
+            this.memory = rnd.Next() % 2049;
+            this.bluetooth = rnd.Next() % 2 == 0 ? "есть" : "нет";
+        }
+        public override string getInfo()
+        {   
+
+            return $"{cores} ядер, {memory} GB памяти, bluetooth {bluetooth}, "+base.getInfo();
+        }
+    }
+    public class Tablet : Gadget
+    {
+        string camera = string.Empty;
+        int touching = 0;
+       public Tablet()
+        {
+            camera = rnd.Next() % 2 == 0 ? "есть" : "нет";
+            touching = 1 + rnd.Next() % 10;
+        }
+        public override string getInfo()
+        {
+            return $"камера {camera}, поддерживаемых касаний одновременно {touching}, " + base.getInfo();
+        }
+    }
+   public class Smartphone : Gadget
+    {
+        byte simslots = 0;
+        byte megapixels = 0;
+        int battery = 0;
+      public Smartphone()
+        {
+            this.simslots = (byte)(1 + rnd.Next() % 2);
+            this.megapixels = (byte)(5 + rnd.Next() % 47);
+            this.battery = (1200+rnd.Next() % 2001);
+        }
+        public override string getInfo()
+        {
+            return $"поддерживаемых симок {simslots}, камера {megapixels} мегапикселей, батарея {battery} mAh, " + base.getInfo();
+        }
+    }
+}
